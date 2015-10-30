8bca90cf8215159edaee62537dbc9f87b27fa7b1
hong qiu <qiuhong4@hotmail.com>
2015-10-30 00:28:25 -0700

    new pilots file

--- Pilltest.playground/Contents.swift
+++ Pilltest.playground/Contents.swift
@@ -24,6 +24,7 @@ import UIKit
 var error: NSError?
 let fileURL = NSBundle.mainBundle().URLForResource("pill", withExtension: "txt")
 let content = String(contentsOfURL: fileURL!, encoding: NSUTF8StringEncoding, error: &error)
+let array = content!.componentsSeparatedByString(", ")
 
 
 
