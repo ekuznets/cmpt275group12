549d413f69c64c7907d0da4a7863c345558dbc45
hong qiu <qiuhong4@hotmail.com>
2015-10-30 00:21:41 -0700

    upload pilltest

---
+++ Pilltest.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+//let file: NSFileHandle? = NSFileHandle(forReadingAtPath: "pill.txt")
+//
+//if file == nil {
+//    println("File open failed")
+//} else {
+//    file?.seekToFileOffset(10)
+//    let databuffer = file?.readDataOfLength(5)
+//    file?.closeFile()
+//}
+
+//let file: NSFileHandle? = NSFileHandle(forReadingAtPath: "Macintoshi/Users/Andrew/Desktop/pill.txt")
+//
+//if file == nil {
+//    println("File open failed")
+//} else {
+//    file?.closeFile()
+//}
+
+
+
+import UIKit
+
+var error: NSError?
+let fileURL = NSBundle.mainBundle().URLForResource("pill", withExtension: "txt")
+let content = String(contentsOfURL: fileURL!, encoding: NSUTF8StringEncoding, error: &error)
+
+
+
+class Pill{
+    //stored properties
+    var name: String
+    var DIN: Int
+    var drugCode: Int
+    
+    init(name: String, DIN: Int, drugCode: Int){
+        self.name = name
+        self.DIN = DIN
+        self.drugCode = drugCode
+    }
+    
+    deinit{
+        // any necessary cleanup code
+    }
+    
+    func pillDescript () -> String {
+        return ""
+    }
+}
