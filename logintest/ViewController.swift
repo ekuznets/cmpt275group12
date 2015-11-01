bb9b03d1a8c9ad74d397cae01669ad293045ce9c
Evgeny KUZNETSOV <ekuznets@sfu.ca>
2015-10-31 20:37:58 -0700

    EK: researchKit tools basic

--- logintest/ViewController.swift
+++ logintest/ViewController.swift
@@ -7,7 +7,30 @@
 //
 
 import UIKit
+import ResearchKit
 
+// http://www.raywenderlich.com/104575/researchkit-tutorial-with-swift usefull stuff for healthkit
+
+extension ViewController : ORKTaskViewControllerDelegate {
+    
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        //Handle results with taskViewController.result
+        taskViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func consentTapped(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func surveyTapped(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: SurveyTask, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
+    }
+    
+}
 
 class ViewController: UIViewController {
 
@@ -31,40 +54,40 @@ class ViewController: UIViewController {
     
     @IBAction func FEEETCH(sender: AnyObject)
     {
-        let url = NSURL(string: "http://52.89.151.217/test.php")
-        
-        let session = NSURLSession.sharedSession()
-
-        let dataTask = NSURLSession.sharedSession().dataTaskWithURL(url!)
-        {
-            (data, response, error) in
-            if error != nil
-            {
-                
-                println(error.description)
-                
-            }
-            else
-            {
-                var responseDict: NSDictionary = NSJSONSerialization.JSONObjectWithData(data,options: NSJSONReadingOptions.MutableContainers, error:nil) as! NSDictionary
-                
-                println("jsonObject :\(responseDict)")
-                let desease = responseDict["diseasetype"] as! String
-                
-                println("Parse: \(desease)")
-                let location = responseDict["location"] as! String
-                println("Parse: \(location)")
-                
-                
-                let arrayInfected: AnyObject! = responseDict["arrayInfected"]![0]
-                let StringInt = arrayInfected["infected"] as! String
-                let int = StringInt.toInt()
-                
-            }
-            
-        }
-        
-        dataTask.resume()
+//        let url = NSURL(string: "http://52.89.151.217/test.php")
+//        
+//        let session = NSURLSession.sharedSession()
+//
+//        let dataTask = NSURLSession.sharedSession().dataTaskWithURL(url!)
+//        {
+//            (data, response, error) in
+//            if error != nil
+//            {
+//                
+//                println(error.description)
+//                
+//            }
+//            else
+//            {
+//                var responseDict: NSDictionary = NSJSONSerialization.JSONObjectWithData(data,options: NSJSONReadingOptions.MutableContainers, error:nil) as! NSDictionary
+//                
+//                println("jsonObject :\(responseDict)")
+//                let desease = responseDict["diseasetype"] as! String
+//                
+//                println("Parse: \(desease)")
+//                let location = responseDict["location"] as! String
+//                println("Parse: \(location)")
+//                
+//                
+//                let arrayInfected: AnyObject! = responseDict["arrayInfected"]![0]
+//                let StringInt = arrayInfected["infected"] as! String
+//                let int = StringInt.toInt()
+//                
+//            }
+//            
+//        }
+//        
+//        dataTask.resume()
         
         
         
