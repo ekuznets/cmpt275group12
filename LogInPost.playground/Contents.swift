58c18348d9e2144d2fe0583e3e5f8ef905920703
Evgeny KUZNETSOV <ekuznets@sfu.ca>
2015-11-01 21:10:36 -0800

    EK: signup, login, logout scripts, play ground version

---
+++ LogInPost.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import XCPlayground
+XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
+
+let myUrl = NSURL(string: "http://52.89.151.217/login.php")
+let request = NSMutableURLRequest(URL:myUrl!)
+request.HTTPMethod = "POST"
+
+// Compose a query string
+let postString = "email=ak@sfu.ca&password=123" // type email and password
+request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+
+let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {(data, response, error) in
+    
+    if error != nil
+    {
+        println("error=\(error)")
+        //return
+    }
+    
+    // You can print out response object
+    println("response = \(response)")
+    
+    // Print out response body
+    let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+    println("responseString = \(responseString)")
+    
+    //Let’s convert response sent from a server side script to a NSDictionary object:
+    
+    var err: NSError?
+    var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
+    
+    if let parseJSON = myJSON {
+        // Now we can access value of First Name by its key
+        var firstNameValue = parseJSON["location"] as? String
+        println("firstNameValue: \(firstNameValue)")
+    }
+    
+}
+
+
+
+task.resume()
